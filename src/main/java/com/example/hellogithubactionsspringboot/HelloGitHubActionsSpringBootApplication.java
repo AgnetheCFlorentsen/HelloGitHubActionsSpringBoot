000035ee9a3d86cb0aecb5d6a993a70de72b4339
Agnethe Florentsen <agfl0001@stud.kea.dk>
2024-02-27 09:56:45 +0100

    Initial commit

---
+++ src/main/java/com/example/hellogithubactionsspringboot/HelloGitHubActionsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactionsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitHubActionsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitHubActionsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellogithubactionsspringboot/HelloGitHubActionsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactionsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitHubActionsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
